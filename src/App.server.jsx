6bad51e470e36f0350ce6dfbc060f3534c78aa3a
Jonathan Junior Calixte <jcalixte@Administrators-MacBook-Pro.local>
2022-09-20 13:36:33 -0400

    folder structure

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
-import Stack from '@mui/material/Stack';
-import Slider from '@mui/material/Slider';
-import VolumeDown from '@mui/icons-material/VolumeDown';
-import VolumeUp from '@mui/icons-material/VolumeUp';
+import HomePage from "./routes/pages/Homepage";
+
 
 
 
@@ -11,8 +9,7 @@ function App() {
 
   return (
  <React.Fragment>
-   {/*<CssBaseline/>*/}
-   <h1 className="bg-amber-300 text-center ">Hello world</h1>
+ <HomePage/>
  </React.Fragment>
   );
 }
---
+++ src/routes/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const HomePage=()=>{
+    return(
+        <h1 className="bg-amber-300 text-center ">Hello world !!!</h1>
+
+    )
+}
+export default HomePage
